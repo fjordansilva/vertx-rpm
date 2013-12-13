c80b2d2371237bbfce48f96883fefc3352e548dc
Fernando Jordan Silva <fjordan@aubay.es>
2013-12-13 10:04:33 +0100

    [ADDED] Build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+set -e
+
+RPMBUILD_DIR=`rpm --eval %{_topdir}`
+
+pushd `dirname $0` > /dev/null
+VERTX_RPM_DIR=$(pwd)
+popd > /dev/null
+
+cd $RPMBUILD_DIR/SPECS
+cp $VERTX_RPM_DIR/*.spec .
+cd -
+
+spectool -g -R $RPMBUILD_DIR/SPECS/vertx.spec
+rpmbuild --clean -ba $RPMBUILD_DIR/SPECS/vertx.spec
